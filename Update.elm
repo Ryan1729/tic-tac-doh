4c248717d56b124568858ea2a4235abb789e7ee1
Ryan1729 <Ryan1729@gmail.com>
2016-11-02 02:55:48 -0600

    have CPU give up when out of moves
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- Update.elm
+++ Update.elm
@@ -3,6 +3,8 @@ module Update exposing (update)
 import Msg exposing (Msg(..))
 import Model exposing (Model, Board, Size, Outcome(..))
 import Material
+import Extras
+import Random.Pcg as Random exposing (Seed)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -31,37 +33,101 @@ placeMap : (Size -> Board -> Board) -> Model -> ( Model, Cmd Msg )
 placeMap placeFunction model =
     case model.selected of
         Just size ->
-            let
-                stashAmount =
-                    Model.stashGet size model.stash
-            in
-                if model.outcome == TBD && stashAmount >= 1 then
-                    let
-                        newBoard =
-                            placeFunction size model.board
+            if model.outcome == TBD && Model.stashGet size model.stash >= 1 then
+                let
+                    postPlacementModel =
+                        applyPlaceFunction placeFunction size model
 
-                        newStashAmount =
-                            stashAmount - 1
+                    newModel =
+                        { postPlacementModel
+                            | outcome = Model.getOutcome postPlacementModel
+                            , player = Model.nextPlayer postPlacementModel.player
+                        }
 
-                        newModel =
-                            { model
-                                | board = newBoard
-                                , stash = Model.stashSet size newStashAmount model.stash
-                                , selected =
-                                    if newStashAmount >= 1 then
-                                        model.selected
-                                    else
-                                        Nothing
-                            }
-                    in
-                        ( { newModel
-                            | outcome = Model.getOutcome newModel
-                            , player = Model.nextPlayer newModel.player
-                          }
-                        , Cmd.none
-                        )
-                else
-                    ( { model | selected = Nothing }, Cmd.none )
+                    finalModel =
+                        case cpuTurn newModel of
+                            Just postCPUModel ->
+                                postCPUModel
+
+                            Nothing ->
+                                { newModel
+                                    | outcome =
+                                        case newModel.outcome of
+                                            TBD ->
+                                                UserWin
+
+                                            otherOutcome ->
+                                                otherOutcome
+                                }
+                in
+                    ( finalModel
+                    , Cmd.none
+                    )
+            else
+                ( { model | selected = Nothing }, Cmd.none )
 
         Nothing ->
             ( model, Cmd.none )
+
+
+applyPlaceFunction : (Size -> Board -> Board) -> Size -> Model -> Model
+applyPlaceFunction placeFunction size model =
+    let
+        newBoard =
+            placeFunction size model.board
+
+        newStashAmount =
+            (Model.stashGet size model.stash) - 1
+    in
+        { model
+            | board = newBoard
+            , stash = Model.stashSet size newStashAmount model.stash
+            , selected =
+                if newStashAmount >= 1 then
+                    model.selected
+                else
+                    Nothing
+        }
+
+
+cpuTurn : Model -> Maybe Model
+cpuTurn model =
+    if model.outcome == TBD && model.player == Model.CPU then
+        let
+            placeFunctions : List (Model -> Model)
+            placeFunctions =
+                --TODO applyPlaceFunction [placeFunctions]
+                []
+                    |> shuffle (Random.initialSeed 42)
+
+            maybeWinningMove =
+                Extras.find (\f -> model |> f |> Model.getOutcome |> (==) Model.CPUWin) placeFunctions
+        in
+            case maybeWinningMove of
+                Just move ->
+                    model
+                        |> move
+                        |> Just
+
+                Nothing ->
+                    let
+                        maybeMove =
+                            Random.step (Random.sample placeFunctions) (Random.initialSeed 42)
+                                |> fst
+                    in
+                        Maybe.map (\move -> move model) maybeMove
+    else
+        Nothing
+
+
+shuffle : Seed -> List a -> List a
+shuffle seed list =
+    let
+        length =
+            List.length list
+
+        randomTags =
+            Random.step (Random.list length (Random.int 0 length)) seed
+                |> fst
+    in
+        List.map2 (,) randomTags list |> List.sortBy fst |> List.unzip |> snd
